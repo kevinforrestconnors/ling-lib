9b65ed5d35e2c92d1a8f99a714c0a2e17dc0c5fb
Kevin Connors <connorsk@grinnell.edu>
2017-03-03 15:03:58 -0600

    add affixes which currently include prefix and suffix matching on atoms ending and beginning with -, respectively

---
+++ pl/affix.pl
@@ -0,0 +1,12 @@
+:- use_module(library(clpfd)).
+
+affix(X):-
+	prefix(X) ; suffix(X). % ; infix(X) ; circumfix(X)
+
+prefix(Affix):-
+	atom_length(Affix, Len),
+	Dash #= Len - 1,
+	sub_atom(Affix, Dash, 1, _, '-').
+
+suffix(Affix):-
+	sub_atom(Affix, 0, 1, _, '-').
