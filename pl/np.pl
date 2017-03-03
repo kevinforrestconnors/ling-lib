bdc53cabd0c04dac79daf0b28c642c66e288258f
Kevin Connors <connorsk@grinnell.edu>
2017-03-03 16:26:58 -0600

    change np to provide noun data / allow specification of noun

--- pl/np.pl
+++ pl/np.pl
@@ -1,17 +1,17 @@
-:- module(np, [np/1, np/2]).
+:- module(np, [np/2, np/3]).
 
 :- use_module(n, [sg_n/1, pl_n/1]).
 :- use_module(det, [sg_det/1, pl_det/1]).
 
-np(R):-
-	np(sg, R) ; np(pl, R).
-np(sg, R):-
+np(N, R):-
+	np(N, sg, R) ; np(N, pl, R).
+np(N, sg, R):-
 	sg_det(Det),
-	sg_n(Noun),
+	sg_n(N),
 	append(Det, [#], T),
-	append(T, Noun, R).
-np(pl, R):-
+	append(T, N, R).
+np(N, pl, R):-
 	pl_det(Det),
-	pl_n(Noun),
+	pl_n(N),
 	append(Det, [#], T),
-	append(T, Noun, R).
+	append(T, N, R).
