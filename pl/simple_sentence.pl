b2156b2ade9cb95b05517325bae3ed77a9663abf
Kevin Connors <connorsk@grinnell.edu>
2017-03-03 16:23:47 -0600

    simple sentence works, but there is no agreement between subject and verb

---
+++ pl/simple_sentence.pl
@@ -0,0 +1,11 @@
+:- module(simple_sentence, [simple_sentence/3]).
+
+:- use_module(library(clpfd)).
+:- use_module(helpers, [concat_words/3]).
+:- use_module(np, [np/1, np/2]).
+:- use_module(vp, [vp/3, transitive_vp/3, intransitive_vp/1]).
+
+simple_sentence(NP, VP, R):-
+	np(NP),
+	vp(V, N, VP),
+	concat_words(NP, VP, R).
