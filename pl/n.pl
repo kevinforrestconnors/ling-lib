84f88a1e10f945dc751ca43b5cfc8791f2d1e092
Kevin Connors <connorsk@grinnell.edu>
2017-03-07 10:44:13 -0600

    conversion from atoms to words with morphemes now works for singular and plural nouns and pronouns

--- pl/n.pl
+++ pl/n.pl
@@ -1,61 +1,77 @@
-:- module(n, [n/1, sg_n/1, pl_n/1, person_of_noun/2, pluralize/2]).
+:- module(n, [n/1, normal_noun/1, pronoun/1, sg_n/1, pl_n/1, person_of_noun/2, pluralize/2]).
 
+:- use_module(morpheme, [add_morpheme/3]).
 :- use_module(det, [det/1, sg_det/1, pl_det/1]).
 
 % Subset of English nouns
 n(X):-
 	normal_noun(X) ; pronoun(X).
 
-normal_noun([man]).
-normal_noun([woman]).
-normal_noun([cat]).
-normal_noun([dog]).
+normal_noun('man').
+normal_noun('woman').
+normal_noun('cat').
+normal_noun('dog').
 
 pronoun(X):-
 	sg_pronoun(X) ; pl_pronoun(X).
 
-sg_pronoun([i]).
-sg_pronoun([you]).
-sg_pronoun([he]).
-sg_pronoun([she]).
-sg_pronoun([they]).
-sg_pronoun([it]).
+sg_pronoun('i').
+sg_pronoun('you').
+sg_pronoun('he').
+sg_pronoun('she').
+sg_pronoun('they').
+sg_pronoun('it').
 
 % pl pronouns
-pl_pronoun([we]).
-pl_pronoun([yall]).
-pl_pronoun([they]).
+pl_pronoun('we').
+pl_pronoun('yall').
+pl_pronoun('they').
 
 % 1st
-fst_person([i]).
-fst_person([we]).
+fst_person('i').
+fst_person('we').
 
 % 2nd
-snd_person([you]).
-snd_person([yall]).
+snd_person('you').
+snd_person('yall').
 
 % 3rd
-trd_person([he]).
-trd_person([she]).
-trd_person([they]).
-trd_person([it]).
+trd_person('he').
+trd_person('she').
+trd_person('they').
+trd_person('it').
 
-person_of_noun(Noun, [-1]):-
+% word_of_noun(+Noun, -Word)
+word_of_noun(Noun, word(Noun, [])).
+
+person_of_noun(Noun, '-1'):-
 	n(Noun),
 	fst_person(Noun).
-person_of_noun(Noun, [-2]):-
+person_of_noun(Noun, '-2'):-
 	n(Noun),
 	snd_person(Noun).
-person_of_noun(Noun, [-3]):-
+person_of_noun(Noun, '-3'):-
 	n(Noun),
 	(trd_person(Noun) ; normal_noun(Noun)).
 
-pluralize(Noun, Plural):-
-	n(Noun),
-	append(Noun, [-pl], Plural).
+% pluralize(word(+Noun, +Morphemes), -Word)
+pluralize(word(Noun, Morphemes), Word):-
+	(normal_noun(Noun) ->
+		add_morpheme(word(Noun, Morphemes), '-pl', Word)
+	;
+		Word = word(Noun, Morphemes)
+	).
 
 sg_n(R):-
-	n(R).
+	(
+		normal_noun(Noun)
+	;	sg_pronoun(Noun)
+	),
+	word_of_noun(Noun, R).
 pl_n(R):-
-	n(R0),
-	pluralize(R0, R).
+	(
+		normal_noun(Noun)
+	;	pl_pronoun(Noun)
+	),
+	word_of_noun(Noun, Word),
+	pluralize(Word, R).
