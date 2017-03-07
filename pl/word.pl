b819eec3e527ef87599f0d6c34a692faeae4f8cc
Kevin Connors <connorsk@grinnell.edu>
2017-03-07 11:38:57 -0600

    move concat_phrases to word.pl and fix display_word/2

--- pl/word.pl
+++ pl/word.pl
@@ -1,10 +1,12 @@
 :- module(word, [
 	word/2, 
 	morphemes_well_formed/1, 
-	display_word/2
+	display_word/2,
+	phrase/1,
+	concat_phrases/3,
+	display_phrase/2
 ]).
 
-:- use_module(helpers, [concat_words/3]).
 :- use_module(affix, [prefix/1, suffix/1]).
 :- use_module(morpheme, [morpheme/1]).
 :- use_module(n, [n/1]).
@@ -28,13 +30,32 @@ morphemes_well_formed(Morphemes):-
 display_word(word(Root, Morphemes), Ortho):-
 	include(prefix, Morphemes, Prefixes),
 	include(suffix, Morphemes, Suffixes),
-	atom_list_concat(Prefixes, WordPrefix),
-	atom_list_concat(Suffixes, WordSuffix),
+	print(Prefixes),
+	print(Suffixes),
+	atomic_list_concat(Prefixes, WordPrefix),
+	atomic_list_concat(Suffixes, WordSuffix),
 	atom_concat(WordPrefix, Root, T),
 	atom_concat(T, WordSuffix, Ortho).
 
 phrase(Words):-
 	forall(member(X, Words), word(X)).
+
+concat_phrases(W1, W2, R):-
+	W1 = word(_, _),
+	W2 = word(_, _),
+	append([W1], [W2], R).
+concat_phrases(W1, P1, R):-
+	W1 = word(_, _),
+	phrase(P1),
+	append([W1], P1, R).
+concat_phrases(P1, W1, R):-
+	W1 = word(_, _),
+	phrase(P1),
+	append(P1, [W1], R).
+concat_phrases(P1, P2, R):-
+	phrase(P1),
+	phrase(P2),
+	append(P1, P2, R).
 
 display_phrase(Phrase, OrthoPhrases):-
 	maplist(display_word, Phrase, OrthoPhrases).
