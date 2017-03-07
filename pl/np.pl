3efdbe812296e2a15a9c8eb421dc962d49b3698f
Kevin Connors <connorsk@grinnell.edu>
2017-03-07 15:15:26 -0600

    det and n now agree in number, and n can be a pronoun with no determiner

--- pl/np.pl
+++ pl/np.pl
@@ -1,14 +1,20 @@
 :- module(np, [np/2, np/3]).
 
 :- use_module(word, [concat_phrases/3]).
-:- use_module(n, [noun/2, normal_noun/1]).
+:- use_module(n, [noun/2, normal_noun/1, pronoun/2]).
 :- use_module(det, [det/3]).
 
 % np(?N, -R)
+% phrase(R)
 np(N, R):-
 	np(N, '-sg', R) ; np(N, '-pl', R).
 np(N, Number, R):-
-	det(_, Number, Det),
-	normal_noun(N),
-	noun(word(N, M), Number),
-	concat_phrases(Det, word(N, M), R).
+	(
+		det(_, Number, Det),
+		normal_noun(N),
+		noun(word(N, M), Number),
+		concat_phrases(Det, word(N, M), R)
+	;
+		pronoun(Word, Number),
+		R = [Word]
+	).
